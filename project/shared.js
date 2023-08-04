ff56409af7158e19236802f5d08b6fb59d594988
ronit singh <ronit2001krish@gmail.com>
2023-08-04 11:34:11 +0530

    Source code

---
+++ project/shared.js
@@ -0,0 +1,71 @@
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButtons = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+var ctaButton = document.querySelector('.main-nav__item--cta');
+
+// console.dir(backdrop.style['background-image']);
+
+// console.dir(backdrop);
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener("click", function () {
+    // modal.style.display = "block";
+    // backdrop.style.display = "block";
+    // modal.className = 'open'; // This will actually overwrite the complete class list
+    modal.classList.add("open");
+    backdrop.style.display = 'block';
+    setTimeout(() => {
+      backdrop.classList.add("open");
+    }, 10);
+    // backdrop.classList.add("open");
+  });
+}
+
+backdrop.addEventListener("click", function () {
+  // mobileNav.style.display = 'none';
+  mobileNav.classList.remove("open");
+  closeModal();
+});
+
+if (modalNoButton) {
+  modalNoButton.addEventListener("click", closeModal);
+}
+
+function closeModal() {
+  //   backdrop.style.display = "none";
+  //   modal.style.display = "none";
+  if (modal) {
+    modal.classList.remove("open");
+  }
+  backdrop.classList.remove("open");
+  setTimeout(() => {
+    backdrop.style.display = 'none';
+  }, 300);
+}
+
+toggleButton.addEventListener("click", function () {
+  // mobileNav.style.display = 'block';
+  // backdrop.style.display = 'block';
+  mobileNav.classList.add("open");
+  backdrop.style.display = 'block';
+  setTimeout(() => {
+    backdrop.classList.add("open");
+  }, 10);
+});
+
+// ctaButton.addEventListener('animationstart',function(event){
+//   console.log('Animation started', event)
+// })
+// ctaButton.addEventListener('animationend',function(event){
+//   console.log('Animation ended', event)
+// })
+// ctaButton.addEventListener('animationiteration',function(event){
+//   console.log('Animation iteration', event)
+// })
+
+
+
+// +91 93030 93939
+// +91 70000 76879
